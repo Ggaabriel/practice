4490bce671d880463a58e6317ec8575432ad7ef4
Ggaabriel <djskilleton@gmail.com>
2023-10-25 02:53:44 +0200

    updated slider

--- src/pages/Gallery/Gallery.tsx
+++ src/pages/Gallery/Gallery.tsx
@@ -17,17 +17,22 @@ const Gallery = (_props: Props) => {
     console.log(active);
     
   const handleScroll = () => {
+
     const container:any = scrollContainerRef.current;
     if (container) {
+
       const scrollTop = container.scrollTop;
       const scrollHeight = container.scrollHeight;
     //   const clientHeight = container.clientHeight;
-      const numberOfBlocks = 15;
+      const numberOfBlocks = 30;
       const blockHeight = scrollHeight / numberOfBlocks;
 
-      const currentBlock = Math.floor((scrollTop+1) / blockHeight);
-
+      const currentBlock = Math.floor((scrollTop) / blockHeight);
+     
+    
       setActive(currentBlock);
+      // container.children[currentBlock+1].children[0].classList.add("bob")
+      
     }
   };
 
@@ -36,7 +41,9 @@ const Gallery = (_props: Props) => {
     if (container) {
       container.addEventListener("scroll", handleScroll);
     }
+    
 
+    
     return () => {
       if (container) {
         container.removeEventListener("scroll", handleScroll);
@@ -47,25 +54,27 @@ const Gallery = (_props: Props) => {
   return (
     <div
       ref={scrollContainerRef}
-      className="gallery m-auto h-fit snap-y snap-mandatory overflow-scroll"
+      className="gallery m-auto  h-fit snap-y snap-mandatory overflow-auto"
     >
-      {[...new Array(15)].map((_, i,a) => {
+      {[...new Array(30)].map((_, i,a) => {
         const isCurrent = i === active;
-
+        
         return (
-          <div key={i} className="h-screen snap-center flex justify-center overflow-hidden">
+          <div key={i} className="h-screen  snap-center flex justify-center overflow-hidden">
             <img
               draggable={false}
              
-              className={`h-[50vh] pointer-events-none ${
+              className={`h-[60vh] max-w-[410px] w-[70vw] pointer-events-none ${
                 isCurrent
-                  ? "relative top-[15vh] scale-100 transition"
+                  ? "relative top-[15vh] scale-100 bob  "
                   : "fixed  top-[19vh] scale-90 "
-              }`}
+              } ${active+1 === i ? " visible":active === i ?" visible" :" invisible "} `}
               style={{
                 transform: isCurrent ? "scale(1.1)" : "none",
                 filter:`hue-rotate(${i*50}deg)`,
-                zIndex: isCurrent ? a.length : i===active+1 ? a.length-1 : i===0? a.length-i-1 : a.length-i
+                zIndex: isCurrent ? a.length : i===active+1 ? a.length-1 : i===0? a.length-i-1 : a.length-i,
+                transition: active + 1 === i ?"500ms" : "",
+                
               }}
               src={tiktok}
               alt=""
